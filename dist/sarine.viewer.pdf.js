332c70fae6c540b635c3091c83a9cb8da1cf6d9b
carmeladi <carmeladi@gmail.com>
2015-02-17 17:40:13 +0200

    add support to mobile browsers

--- dist/sarine.viewer.pdf.js
+++ dist/sarine.viewer.pdf.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.pdf - v0.0.6 -  Sunday, February 15th, 2015, 5:08:57 PM 
+sarine.viewer.pdf - v0.0.7 -  Tuesday, February 17th, 2015, 5:39:57 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
@@ -24,14 +24,16 @@ sarine.viewer.pdf - v0.0.6 -  Sunday, February 15th, 2015, 5:08:57 PM
     };
 
     PDF.prototype.first_init = function() {
-      var defer;
+      var defer, htmlVal;
       defer = $.Deferred();
       this.embed.attr({
         src: this.src + this.pdfName,
         type: 'application/pdf',
         width: '100%',
-        height: '100%'
+        height: '90%'
       });
+      htmlVal = "<p>It appears you do not have a PDF plugin for this browser.<br />No biggie... you can <a href='" + this.src + this.pdfName + "'>click here to download the PDF file.</a></p>";
+      this.embed.html(htmlVal);
       defer.resolve(this);
       return defer;
     };
