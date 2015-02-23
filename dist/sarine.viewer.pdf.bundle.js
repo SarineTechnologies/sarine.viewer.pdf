4346883662f62df8a0890f43e142312fdbbf44ae
carmeladi <carmeladi@gmail.com>
2015-02-23 15:54:26 +0200

    change fallback text

--- dist/sarine.viewer.pdf.bundle.js
+++ dist/sarine.viewer.pdf.bundle.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.pdf - v0.0.12 -  Monday, February 23rd, 2015, 3:50:56 PM 
+sarine.viewer.pdf - v0.0.13 -  Monday, February 23rd, 2015, 3:54:12 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
