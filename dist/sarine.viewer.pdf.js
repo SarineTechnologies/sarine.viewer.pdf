70241276078fae9054bda65e586b956677428f3a
adica <carmeladi@gmail.com>
2015-11-12 14:05:15 +0200

    change max-width to width (calculate scale)

--- dist/sarine.viewer.pdf.js
+++ dist/sarine.viewer.pdf.js
@@ -1,11 +1,12 @@
 
 /*!
-sarine.viewer.pdf - v0.8.0 -  Monday, September 21st, 2015, 9:55:02 AM 
+sarine.viewer.pdf - v0.8.0 -  Thursday, November 12th, 2015, 11:39:28 AM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
 (function() {
   var PDF,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
@@ -13,6 +14,7 @@ sarine.viewer.pdf - v0.8.0 -  Monday, September 21st, 2015, 9:55:02 AM
     __extends(PDF, _super);
 
     function PDF(options) {
+      this.scaleImage = __bind(this.scaleImage, this);
       PDF.__super__.constructor.call(this, options);
       this.pdfName = options.pdfName, this.limitSize = options.limitSize;
       this.limitSize = this.limitSize || 250;
@@ -29,15 +31,16 @@ sarine.viewer.pdf - v0.8.0 -  Monday, September 21st, 2015, 9:55:02 AM
       _t = this;
       this.previewSrc = this.fullSrc.indexOf('?') === -1 ? this.fullSrc + '.png' : this.fullSrc.split('?')[0] + '.png?' + this.fullSrc.split('?')[1];
       return this.loadImage(this.previewSrc).then(function(img) {
-        var image, imgName, styleAttr;
+        var image, imgDimensions, imgName;
         image = $("<img>");
         imgName = 'PDF-thumb';
-        styleAttr = 'max-width:' + _t.limitSize + 'px;max-height:' + _t.limitSize + 'px;';
+        imgDimensions = _t.scaleImage(img);
         image.attr({
           src: img.src,
           alt: imgName,
           "class": imgName,
-          style: styleAttr
+          width: imgDimensions.width,
+          height: imgDimensions.height
         });
         if (img.src === _t.callbackPic) {
           image.addClass('no_stone');
@@ -49,10 +52,28 @@ sarine.viewer.pdf - v0.8.0 -  Monday, September 21st, 2015, 9:55:02 AM
               return window.open(_t.fullSrc, '_blank');
             };
           })(this));
-          image.attr('style', image.attr('style') + 'cursor:pointer;');
+          image.attr({
+            'style': 'cursor:pointer;'
+          });
         }
         return defer.resolve(_t);
       });
+    };
+
+    PDF.prototype.scaleImage = function(img) {
+      var biggerDimention, imgDimensions, scale;
+      imgDimensions = {};
+      biggerDimention = img.width > img.height;
+      if (biggerDimention) {
+        scale = img.width / this.limitSize;
+        imgDimensions.width = this.limitSize;
+        imgDimensions.height = img.height / scale;
+      } else {
+        scale = img.height / this.limitSize;
+        imgDimensions.height = this.limitSize;
+        imgDimensions.width = img.width / scale;
+      }
+      return imgDimensions;
     };
 
     PDF.prototype.full_init = function() {
