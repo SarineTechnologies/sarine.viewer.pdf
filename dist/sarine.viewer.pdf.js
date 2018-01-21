e06a9388d9f31b0f71d6a3ce80ab2842ef12505c
Unknown <rivkas@3base.co.il>
2018-01-21 11:59:55 +0200

    return to viewer of pdf.js

--- dist/sarine.viewer.pdf.js
+++ dist/sarine.viewer.pdf.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.pdf - v0.14.7 -  Thursday, January 18th, 2018, 3:13:42 PM 
+sarine.viewer.pdf - v0.14.7 -  Sunday, January 21st, 2018, 11:38:48 AM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
@@ -19,7 +19,6 @@ sarine.viewer.pdf - v0.14.7 -  Thursday, January 18th, 2018, 3:13:42 PM
       PDF.__super__.constructor.call(this, options);
       this.pdfName = options.pdfName, this.limitSize = options.limitSize, this.mode = options.mode;
       this.limitSize = this.limitSize || 250;
-      this.baseUrl = options.baseUrl + "atomic/v1/assets/";
     }
 
     PDF.prototype.convertElement = function() {
@@ -75,7 +74,7 @@ sarine.viewer.pdf - v0.14.7 -  Thursday, January 18th, 2018, 3:13:42 PM
     };
 
     PDF.prototype.initPopup = function(src) {
-      var closeButton, iframeElement, loader, next, openAsLink, pagerDiv, pdfContainer, pdfContainerInside, pdfDiv, prev, sliderWrap, _t;
+      var closeButton, domain, iframeElement, openAsLink, pdfContainer, pdfContainerInside, pdfDiv, pdfUrl, sliderHeight, sliderWrap, url, viewer, _t;
       _t = this;
       sliderWrap = $("body").children().first();
       pdfContainer = $('#iframe-pdf-container');
@@ -84,29 +83,28 @@ sarine.viewer.pdf - v0.14.7 -  Thursday, January 18th, 2018, 3:13:42 PM
       if (pdfContainer.length === 0) {
         pdfContainer = $('<div id="iframe-pdf-container" class="pdf-popup-container">');
         pdfContainerInside = $('<div class="iframe-pdf-inside-container ">');
-        pdfDiv = $('<div class="iframe-pdf-div" style="display:none">');
+        pdfDiv = $('<div class="iframe-pdf-div">');
         if (Device.isMobileOrTablet()) {
           pdfContainerInside.addClass('mobile');
         }
         if (_t.inIframe()) {
           pdfContainer.addClass('iframe-pdf-container-hide');
         }
-        prev = '<svg class="icon icon-angle-left"><path d="M15.429 9v1.286c0 0.683-0.452 1.286-1.175 1.286h-7.071l2.943 2.953c0.241 0.231 0.382 0.563 0.382 0.904s-0.141 0.673-0.382 0.904l-0.753 0.763c-0.231 0.231-0.563 0.372-0.904 0.372s-0.673-0.141-0.914-0.372l-6.539-6.549c-0.231-0.231-0.372-0.563-0.372-0.904s0.141-0.673 0.372-0.914l6.539-6.529c0.241-0.241 0.573-0.382 0.914-0.382s0.663 0.141 0.904 0.382l0.753 0.743c0.241 0.241 0.382 0.573 0.382 0.914s-0.141 0.673-0.382 0.914l-2.943 2.943h7.071c0.723 0 1.175 0.603 1.175 1.286z"></path></svg>';
-        next = '<svg class="icon icon-angle-right"><path d="M14.786 9.643c0 0.342-0.131 0.673-0.372 0.914l-6.539 6.539c-0.241 0.231-0.573 0.372-0.914 0.372s-0.663-0.141-0.904-0.372l-0.753-0.753c-0.241-0.241-0.382-0.573-0.382-0.914s0.141-0.673 0.382-0.914l2.943-2.943h-7.071c-0.723 0-1.175-0.603-1.175-1.286v-1.286c0-0.683 0.452-1.286 1.175-1.286h7.071l-2.943-2.953c-0.241-0.231-0.382-0.563-0.382-0.904s0.141-0.673 0.382-0.904l0.753-0.753c0.241-0.241 0.563-0.382 0.904-0.382s0.673 0.141 0.914 0.382l6.539 6.539c0.241 0.231 0.372 0.563 0.372 0.904z"></path></svg>';
-        pagerDiv = '<div id="pdf-pager">  <button id="prev">' + prev + '</button>  <button id="next">' + next + '</button></div>';
-        iframeElement = $('<div id="iframe-pdf"><canvas id="the-canvas"></canvas></div>');
+        sliderHeight = sliderWrap.height();
+        iframeElement = $('<iframe id="iframe-pdf" frameborder=0  type="application/pdf"></iframe>');
+        domain = window.stones[0].viewersBaseUrl.split('/content')[0];
+        viewer = "/web-sites/pdf-viewer-js/web/viewer.html?file=";
+        url = [domain, viewer, src];
+        pdfUrl = url.join("");
+        iframeElement.attr('src', pdfUrl);
         closeButton = $('<input type="button" value="Close" id="closePdfReport" class="close-popup-report"/>');
         openAsLink = $('<div class="open-pdf-link-container"><a href="' + src + '" target="_blank" id="open-pdf-link"  ><svg class="icon icon-external-link"> <title>external-link</title> <path d="M14.143 9.321v3.214c0 1.597-1.296 2.893-2.893 2.893h-8.357c-1.597 0-2.893-1.296-2.893-2.893v-8.357c0-1.597 1.296-2.893 2.893-2.893h7.071c0.181 0 0.321 0.141 0.321 0.321v0.643c0 0.181-0.141 0.321-0.321 0.321h-7.071c-0.884 0-1.607 0.723-1.607 1.607v8.357c0 0.884 0.723 1.607 1.607 1.607h8.357c0.884 0 1.607-0.723 1.607-1.607v-3.214c0-0.181 0.141-0.321 0.321-0.321h0.643c0.181 0 0.321 0.141 0.321 0.321zM18 0.643v5.143c0 0.352-0.291 0.643-0.643 0.643-0.171 0-0.331-0.070-0.452-0.191l-1.768-1.768-6.549 6.549c-0.060 0.060-0.151 0.1-0.231 0.1s-0.171-0.040-0.231-0.1l-1.145-1.145c-0.060-0.060-0.1-0.151-0.1-0.231s0.040-0.171 0.1-0.231l6.549-6.549-1.768-1.768c-0.121-0.121-0.191-0.281-0.191-0.452 0-0.352 0.291-0.643 0.643-0.643h5.143c0.352 0 0.643 0.291 0.643 0.643z"></path> </svg></a></div>');
-        loader = '<div class="loader popup-loader"><div class="pre-load"></div></div>';
         pdfDiv.append(openAsLink);
-        openAsLink.append(pagerDiv);
         pdfDiv.append(iframeElement);
         pdfDiv.append(closeButton);
         pdfContainerInside.append(pdfDiv);
-        pdfContainerInside.append(loader);
         pdfContainer.append(pdfContainerInside);
         sliderWrap.before(pdfContainer);
-        _t.render_pdf(src);
       }
       pdfContainer.css('display', 'block');
       return closeButton.on('click', ((function(_this) {
@@ -157,110 +155,6 @@ sarine.viewer.pdf - v0.14.7 -  Thursday, January 18th, 2018, 3:13:42 PM
     PDF.prototype.play = function() {};
 
     PDF.prototype.stop = function() {};
-
-    PDF.prototype.onPrevPage = function() {
-      var _this;
-      _this = this;
-      if (_this.pageNum <= 1) {
-        return;
-      }
-      _this.pageNum--;
-      if (_this.pageNum <= 1) {
-        $("#prev").addClass("icon-disable");
-        $("#next").removeClass("icon-disable");
-      } else {
-        $("#prev").removeClass("icon-disable");
-      }
-      return _this.queueRenderPage(_this.pageNum);
-    };
-
-    PDF.prototype.onNextPage = function() {
-      var _this;
-      _this = this;
-      if (_this.pageNum >= _this.pdfDoc.numPages) {
-        return;
-      }
-      _this.pageNum++;
-      if (_this.pageNum >= _this.pdfDoc.numPages) {
-        $("#next").addClass("icon-disable");
-        $("#prev").removeClass("icon-disable");
-      } else {
-        $("#next").removeClass("icon-disable");
-      }
-      return _this.queueRenderPage(_this.pageNum);
-    };
-
-    PDF.prototype.render_pdf = function(url) {
-      var assets, _t;
-      _t = this;
-      _t.pdfDoc = null;
-      _t.pageNum = 1;
-      _t.pageRendering = false;
-      _t.pageNumPending = null;
-      _t.scale = 0.8;
-      _t.canvas = document.getElementById('the-canvas');
-      _t.ctx = _t.canvas.getContext('2d');
-      assets = [
-        {
-          element: 'script',
-          src: '//mozilla.github.io/pdf.js/build/pdf.js'
-        }
-      ];
-      return _t.loadAssets(assets, function() {
-        PDFJS.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
-        return PDFJS.getDocument(url).then(function(pdfDoc_) {
-          _t.pdfDoc = pdfDoc_;
-          if (_t.pdfDoc.numPages > 1) {
-            document.getElementById('prev').addEventListener('click', _t.onPrevPage.bind(_t));
-            document.getElementById('next').addEventListener('click', _t.onNextPage.bind(_t));
-          } else {
-            $("#pdf-pager").css('display', 'none');
-          }
-          $("#prev").addClass("icon-disable");
-          return _t.renderPage(_t.pageNum);
-        });
-      });
-    };
-
-    PDF.prototype.renderPage = function(num) {
-      var _t;
-      _t = this;
-      _t.pageRendering = true;
-      return _t.pdfDoc.getPage(num).then(function(page) {
-        var desiredWidth, renderContext, renderTask, scale, scaledViewport, viewport;
-        viewport = page.getViewport(1);
-        desiredWidth = document.getElementsByClassName("iframe-pdf-inside-container")[0].offsetWidth;
-        scale = desiredWidth / viewport.width;
-        scaledViewport = page.getViewport(scale);
-        _t.canvas.height = scaledViewport.height;
-        _t.canvas.width = scaledViewport.width;
-        renderContext = {
-          canvasContext: _t.ctx,
-          viewport: scaledViewport
-        };
-        $(".popup-loader").css("display", "block");
-        renderTask = page.render(renderContext);
-        return renderTask.promise.then(function() {
-          _t.pageRendering = false;
-          $(".iframe-pdf-div").css("display", "block");
-          $(".popup-loader").css("display", "none");
-          if (_t.pageNumPending !== null) {
-            _t.renderPage(_t.pageNumPending);
-            return _t.pageNumPending = null;
-          }
-        });
-      });
-    };
-
-    PDF.prototype.queueRenderPage = function(num) {
-      var _t;
-      _t = this;
-      if (_t.pageRendering) {
-        return _t.pageNumPending = num;
-      } else {
-        return _t.renderPage(num);
-      }
-    };
 
     return PDF;
 
