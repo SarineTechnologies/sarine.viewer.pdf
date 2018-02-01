3fb392a49c87b23718cd63ee8e6997855a0d2be5
ronishoham <roni.shoham@sarine.com>
2018-02-01 12:06:06 +0200

    resolved canvas bug and scrolling

--- coffee/sarine.viewer.pdf.coffee
+++ coffee/sarine.viewer.pdf.coffee
@@ -1,5 +1,5 @@
 ###!
-sarine.viewer.pdf - v0.15.7 -  Wednesday, January 31st, 2018, 3:50:29 PM 
+sarine.viewer.pdf - v0.15.9 -  Thursday, February 1st, 2018, 11:20:34 AM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
 ###
 class PDF extends Viewer
@@ -67,7 +67,7 @@ class PDF extends Viewer
 </svg></a></div>')
 
 			pdfDiv.append openAsLink
-			#pdfDiv.append iframeElement
+			pdfDiv.append iframeElement
 			pdfDiv.append closeButton
 
 			pdfContainerInside.append pdfDiv
@@ -82,7 +82,7 @@ class PDF extends Viewer
 
 
 		pdfContainer.css 'display', 'block'
-		$(".slider-wrap,.dashboard").addClass('prevent_scroll')
+		$(".dashboard").hide()
 		if (Device.isMobileOrTablet() and isSafari)
 			$(".dashboard").css("height",window.innerHeight-100+"px")
 			iframeElement.css("height",window.innerHeight-300+"px")
@@ -90,14 +90,9 @@ class PDF extends Viewer
 			$(".dashboard").css("height",window.innerHeight+"px")
 
 		closeButton.on 'click', (=>
-				pdfContainer.css 'display', 'none'
-				$(".slider-wrap,.dashboard").removeClass('prevent_scroll')
-				$(".dashboard").css("height","auto")
-				if(isSafari)
-					$('canvas').each((index , element) ->
-						element.width +=0
-					)
-				return
+			pdfContainer.css 'display', 'none'
+			$(".dashboard").show()
+			return
 			)	
 
 	inIframe :()->
--- dist/sarine.viewer.pdf.bundle.js
+++ dist/sarine.viewer.pdf.bundle.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.pdf - v0.15.7 -  Wednesday, January 31st, 2018, 3:50:29 PM 
+sarine.viewer.pdf - v0.15.9 -  Thursday, February 1st, 2018, 11:20:33 AM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
@@ -201,24 +201,17 @@ sarine.viewer.pdf - v0.15.7 -  Wednesday, January 31st, 2018, 3:50:29 PM
         }
       }
       pdfContainer.css('display', 'block');
-      $(".slider-wrap,.dashboard").addClass('prevent_scroll');
+      $(".dashboard").hide();
       if (Device.isMobileOrTablet() && isSafari) {
         $(".dashboard").css("height", window.innerHeight - 100 + "px");
         iframeElement.css("height", window.innerHeight - 300 + "px");
       } else {
         $(".dashboard").css("height", window.innerHeight + "px");
-        iframeElement.css("height", window.innerHeight - 200 + "px");
       }
       return closeButton.on('click', ((function(_this) {
         return function() {
           pdfContainer.css('display', 'none');
-          $(".slider-wrap,.dashboard").removeClass('prevent_scroll');
-          $(".dashboard").css("height", "auto");
-          if (isSafari) {
-            $('canvas').each(function(index, element) {
-              return element.width += 0;
-            });
-          }
+          $(".dashboard").show();
         };
       })(this)));
     };
